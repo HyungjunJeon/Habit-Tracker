ea436121900b618230d4f22e01831e182b7247ae
HyungjunJeon <maru105kr@gmail.com>
2020-11-23 16:38:48 +0900

    Create HabitAddForm Component

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
 import Habit from "./habit";
+import HabitAddForm from "./habitAddForm";
 
 class Habits extends Component {
   render() {
     return (
-      <ul>
-        {this.props.habits.map((habit) => {
-          return (
-            <Habit
-              key={habit.id}
-              habit={habit}
-              onIncrement={this.props.onIncrement}
-              onDecrement={this.props.onDecrement}
-              onDelete={this.props.onDelete}
-            />
-          );
-        })}
-      </ul>
+      <>
+        <HabitAddForm onAdd={this.props.onAdd} />
+        <ul>
+          {this.props.habits.map((habit) => {
+            return (
+              <Habit
+                key={habit.id}
+                habit={habit}
+                onIncrement={this.props.onIncrement}
+                onDecrement={this.props.onDecrement}
+                onDelete={this.props.onDelete}
+              />
+            );
+          })}
+        </ul>
+      </>
     );
   }
 }
